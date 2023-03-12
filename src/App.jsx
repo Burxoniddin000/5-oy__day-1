d05da569c9c0fe6273068a79cb3a4f2cc3a08e86
Burxon1dd1n <sotvoldiyevburxoniddin5@gmail.com>
2023-03-12 15:46:15 +0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import './main.scss'
 import search from './search.svg'
 import grmaniya from './Germany.png'
 
+let array = []
+
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(element => {
+
+      array.push(element)
+    });
+    console.log(array);
+  })
+
+
 function App() {
   return (
     <>
@@ -21,99 +34,38 @@ function App() {
 
       <main className="main">
         <div className="container">
-        <section className="main__section">
-          <div className="main__inner">
-            <img src={search} alt="sarch" />
-            <input type="text" className="main__input" />
-          </div>
-          <div className="main__select">
-          <select className="input__select" name="cars" id="cars">
-                    <option value="All">All</option>
-                    <option value="Africa">Africa</option>
-                    <option value="America">America</option>
-                    <option value="Asia">Asia</option>
-                    <option value="Europe">Europe</option>
-                    <option value="Oceania">Oceania</option>
-                    <option value="ame">Filter by Region</option>
-            </select>
-          </div>
-        </section>
-        <section>
+          <section className="main__section">
+            <div className="main__inner">
+              <img src={search} alt="sarch" />
+              <input type="text" className="main__input" />
+            </div>
+            <div className="main__select">
+              <select className="input__select" name="cars" id="cars">
+                <option value="All">All</option>
+                <option value="Africa">Africa</option>
+                <option value="America">America</option>
+                <option value="Asia">Asia</option>
+                <option value="Europe">Europe</option>
+                <option value="Oceania">Oceania</option>
+                <option value="ame">Filter by Region</option>
+              </select>
+            </div>
+          </section>
+          <section>
           <ul className="main__ul">
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-            <li className="item">
-              <img src={grmaniya} alt="" />
-              <div className="item__div">
-              <h2 className="item__name">Germany</h2>
-              <p className="item__populat">Population: 81,770,900</p>
-              <p className="item__populat">Region: Europe</p>
-              <p className="item__populat">Capital: Berlin</p>
-              </div>
-            </li>
-              <li className="item">
-                <img src={grmaniya} alt="" />
-                <div className="item__div">
-                <h2 className="item__name">Germany</h2>
-                <p className="item__populat">Population: 81,770,900</p>
-                <p className="item__populat">Region: Europe</p>
-                <p className="item__populat">Capital: Berlin</p>
-                </div>
-              </li>
-          </ul>
-        </section>
+            {array.map((post, i) => (
+                <li  key={i}  className="item">
+                  <img className="img" src={post.flags.png} alt="" />
+                  <div className="item__div">
+                  <h2 className="item__name">{post.name.common}</h2>
+                    <p className="item__populat">Population: {post.population}</p>
+                    <p className="item__populat">Region: {post.region}</p>
+                    <p className="item__populat">Capital: {post.capital}</p>
+                  </div>
+                </li>
+            ))}
+            </ul>
+          </section>
         </div>
       </main>
     </>
